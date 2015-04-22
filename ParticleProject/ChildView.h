3859614dbe308a4272086c0759c06be1666c997b
njd911 <njdhockeydude@gmail.com>
2015-04-21 20:15:56 -0400

    Initial commit

---
+++ ParticleProject/ChildView.h
@@ -0,0 +1,36 @@
+
+// ChildView.h : interface of the CChildView class
+//
+
+
+#pragma once
+
+#include "graphics/OpenGLWnd.h"
+
+// CChildView window
+
+class CChildView : public COpenGLWnd
+{
+// Construction
+public:
+	CChildView();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Implementation
+public:
+	virtual ~CChildView();
+
+	// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
---
+++ ParticleProject/ParticleProject.h
@@ -0,0 +1,35 @@
+
+// ParticleProject.h : main header file for the ParticleProject application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CParticleProjectApp:
+// See ParticleProject.cpp for the implementation of this class
+//
+
+class CParticleProjectApp : public CWinAppEx
+{
+public:
+	CParticleProjectApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CParticleProjectApp theApp;
